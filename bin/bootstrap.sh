76d662c09dac730420a6515adc8e2aeb159a8adb
mo khan <mo@mokhan.ca>
2014-12-29 08:44:59 -0700

    do an update after install pg repo.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -23,6 +23,7 @@ apt-get -y install nginx
 
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt-get -y update
 apt-get install -y postgresql-9.4 libpq-dev
 
 curl -sL https://deb.nodesource.com/setup | sudo bash -

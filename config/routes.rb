aa1fa8d952babdcea1c29763e52073ba3b87ca1c
Stephen Peasley <s@stephenpeasley.com>
2015-01-17 08:18:30 -0700

    Add Terms page, route and links

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :registrations, only: [:new, :create]
   get '/' => 'sessions#new', as: :dashboard
+  get '/terms' => 'static_pages#terms'
 end
--- spec/routing/route_routing_spec.rb
+++ spec/routing/route_routing_spec.rb
@@ -5,3 +5,9 @@ describe "root route" do
     expect(get: '/').to route_to(controller: 'sessions', action: 'new')
   end
 end
+
+describe "terms route" do
+  it 'routes to the terms and conditions page' do
+    expect(get: '/terms').to route_to(controller: 'static_pages', action: 'terms')
+  end
+end

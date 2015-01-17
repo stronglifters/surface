b9b852b7c366e8f32a2b9a3c4592d08543c3c4dc
mo khan <mokhan@users.noreply.github.com>
2015-01-17 09:23:26 -0700

    double quotes.

--- spec/routing/route_routing_spec.rb
+++ spec/routing/route_routing_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe "root route" do
-  it 'routes to the login page' do
+  it "routes to the login page" do
     expect(get: '/').to route_to(controller: 'sessions', action: 'new')
   end
 end
 
 describe "terms route" do
-  it 'routes to the terms and conditions page' do
+  it "routes to the terms and conditions page" do
     expect(get: '/terms').
       to route_to(controller: 'static_pages', action: 'terms')
   end

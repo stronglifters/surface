de29dfd4cf6e36b50666c9687dceb3a297683589
mo khan <mo@mokhan.ca>
2015-01-20 22:13:39 -0700

    hound happy.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe User do
   describe "#create" do
     it "saves a new user to the database" do
-      user = User.create!(username: "blah", email: "blah@example.com", password: "password")
+      user = create(:user)
 
       saved_user = User.find(user.id)
-      expect(saved_user.username).to eql("blah")
-      expect(saved_user.email).to eql("blah@example.com")
+      expect(saved_user.username).to eql(user.username)
+      expect(saved_user.email).to eql(user.email)
       expect(saved_user.password).to be_nil
     end
   end
@@ -28,9 +28,9 @@ describe User do
 
       it "is invalid if the username is already taken" do
         User.create(username: "blah", email: "blah@example.com")
-        second_user = User.create(username: "blah", email: "blahblah@example.com")
+        user = User.create(username: "blah", email: "blahblah@example.com")
 
-        expect(second_user.errors[:username]).to_not be_empty
+        expect(user.errors[:username]).to_not be_empty
       end
     end
 

f9d853a106e8aed0dbee8a6bc944100b57a556fb
mo khan <mo@mokhan.ca>
2015-01-20 22:30:41 -0700

    happy hound.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_mailer.default_options = { from: 'from@example.com' }
+  config.action_mailer.default_options = { from: "from@example.com" }
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false

7a7d54df1236dd4aaf66b8b2904183309aa85891
mo khan <mo@mokhan.ca>
2015-02-28 10:06:24 -0700

    scaffold up the items.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :registrations, only: [:new, :create]
+  resources :items
   get '/' => 'sessions#new', as: :dashboard
   get "/terms" => "static_pages#terms"
 end

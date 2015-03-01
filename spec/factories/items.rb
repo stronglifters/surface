c669447bb0c867507f6e25413133a31b2c845616
mo khan <mo@mokhan.ca>
2015-03-01 08:42:42 -0700

    hound happy.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name Faker::Name.name
     description Faker::HipsterIpsum.words(50)
     serial_number SecureRandom.uuid
-    purchase_price { (rand(1_000) + rand()).round(2) }
+    purchase_price { (rand(1_000) + rand).round(2) }
     purchased_at DateTime.now
   end
 end

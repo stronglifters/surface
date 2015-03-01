a52ca21c29f91dda03a51651d92013a94598dc79
mo khan <mo@mokhan.ca>
2015-03-01 08:43:18 -0700

    replace single quotes with double.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root "sessions#new"
   resources :sessions, only: [:new, :create, :destroy]
   resources :registrations, only: [:new, :create]
   resources :items
-  get '/dashboard' => 'items#index', as: :dashboard
+  get "/dashboard" => "items#index", as: :dashboard
   get "/terms" => "static_pages#terms"
 end

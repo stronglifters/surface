c39da7594cacb0dcee7b0b0226f3f6162e03c603
mo khan <mo@mokhan.ca>
2015-03-14 12:24:50 -0600

    add spec for adding a new item from the dashboard.

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -7,12 +7,17 @@ feature "items", type: :feature do
     let!(:item) { create(:item, user: user) }
 
     before :each do
-      subject.login_with(user.username, 'password')
+      subject.login_with(user.username, "password")
       subject.visit_page
     end
 
     it "loads a list of items" do
       expect(page).to have_content(item.name)
+    end
+
+    it "can start to add a new item" do
+      subject.add_item("new item")
+      expect(page.find("#item_name")).to have_val("new item")
     end
   end
 end
--- spec/support/pages/items_page.rb
+++ spec/support/pages/items_page.rb
@@ -4,4 +4,11 @@ class ItemsPage < PageModel
   def initialize
     super dashboard_path
   end
+
+  def add_item(item_name)
+    within "#new_item" do
+      fill_in I18n.translate("items.index.item_name"), with: item_name
+      click_button I18n.translate("items.index.new_item_button")
+    end
+  end
 end

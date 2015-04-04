ebc1c3aab52c2369d6d82e3f3f9c777e8bcbd261
mo khan <mo@mokhan.ca>
2015-04-04 09:44:07 -0600

    add spec to ensure you are redirected to the login page.

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -6,18 +6,27 @@ feature "items", type: :feature do
     let(:user) { create(:user) }
     let!(:item) { create(:item, user: user) }
 
-    before :each do
-      subject.login_with(user.username, "password")
-      subject.visit_page
+    context "when logged in" do
+      before :each do
+        subject.login_with(user.username, "password")
+        subject.visit_page
+      end
+
+      it "loads a list of items" do
+        expect(page).to have_content(item.name)
+      end
+
+      it "can start to add a new item" do
+        subject.add_item("new item")
+        expect(page).to have_selector("input[value='new item']")
+      end
     end
 
-    it "loads a list of items" do
-      expect(page).to have_content(item.name)
-    end
-
-    it "can start to add a new item" do
-      subject.add_item("new item")
-      expect(page).to have_selector("input[value='new item']")
+    context "not logged in" do
+      it 'redirects you to the login page' do
+        subject.visit_page
+        expect(page.current_path).to eql(new_session_path)
+      end
     end
   end
 

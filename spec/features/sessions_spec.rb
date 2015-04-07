078805db94e65acd204efbbefeb788b5e5555492
mo khan <mo@mokhan.ca>
2015-04-06 20:25:58 -0600

    dump the html

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -8,6 +8,7 @@ feature "Sessions", type: :feature, js: true do
     context "credentials are correct" do
       it "takes you to your dashboard" do
         subject.visit_page
+        puts page.html
         subject.login_with(user.username, "password")
         expect(current_path).to eql(dashboard_path)
       end

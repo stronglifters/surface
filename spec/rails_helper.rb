a6496b9f47de333032a060639ca282e9509e96dd
mo khan <mo@mokhan.ca>
2015-04-11 15:12:23 -0600

    download google web font.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,8 +27,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 Capybara.register_driver :poltergeist do |app|
-  options = { debug: true }
-  Capybara::Poltergeist::Driver.new(app, options)
+  Capybara::Poltergeist::Driver.new(app, { debug: true, js_errors: true })
 end
 Capybara.javascript_driver = :poltergeist
 

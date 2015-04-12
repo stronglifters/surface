10b5f659b54306eb8c369b47199d1329248f7deb
mo khan <mo@mokhan.ca>
2015-04-12 13:01:46 -0600

    add coverage options.

--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -79,7 +79,6 @@ Teaspoon.configure do |config|
     # Hooks allow you to use `Teaspoon.hook("fixtures")` before, after, or during your spec run. This will make a
     # synchronous Ajax request to the server that will call all of the blocks you've defined for that hook name.
     #suite.hook :fixtures, proc{ }
-
   end
 
   # Example suite. Since we're just filtering to files already within the root test/javascripts, these files will also
@@ -160,11 +159,11 @@ Teaspoon.configure do |config|
   #config.use_coverage = nil
 
   config.coverage do |coverage|
-
     # Which coverage reports Instanbul should generate. Correlates directly to what Istanbul supports.
     #
     # Available: text-summary, text, html, lcov, lcovonly, cobertura, teamcity
     #coverage.reports = ["text-summary", "html"]
+    coverage.reports = ['text', 'html', 'lcov', 'cobertura']
 
     # The path that the coverage should be written to - when there's an artifact to write to disk.
     # Note: Relative to `config.root`.
@@ -172,11 +171,9 @@ Teaspoon.configure do |config|
 
     # Various thresholds requirements can be defined, and those thresholds will be checked at the end of a run. If any
     # aren't met the run will fail with a message. Thresholds can be defined as a percentage (0-100), or nil.
-    #coverage.statements = nil
-    #coverage.functions = nil
-    #coverage.branches = nil
-    #coverage.lines = nil
-
+    coverage.statements = 80
+    coverage.functions = 80
+    coverage.branches = 80
+    coverage.lines = 80
   end
-
 end

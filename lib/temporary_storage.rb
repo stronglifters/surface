66e3fc00d7bacf0da2b18ef203b13ac1df5e4bd9
mo khan <mo@mokhan.ca>
2015-05-22 14:13:06 -0600

    parameterize the destination filename.

--- lib/temporary_storage.rb
+++ lib/temporary_storage.rb
@@ -1,6 +1,7 @@
 class TemporaryStorage
   def store(file)
-    "#{tmp_dir}/#{file.original_filename}".tap do |new_path|
+    "#{tmp_dir}/#{file.original_filename.parameterize}".tap do |new_path|
+      Rails.logger.info("Copying... #{file.path} to #{new_path}")
       FileUtils.mv(file.path, new_path)
     end
   end

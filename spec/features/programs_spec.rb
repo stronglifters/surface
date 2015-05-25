1bb83285ac714c0bcadae47498e7310800c65752
mo khan <mo@mokhan.ca>
2015-05-24 18:33:45 -0600

    hound happy.

--- spec/features/programs_spec.rb
+++ spec/features/programs_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 feature "Programs", type: :feature do
   include_context "stronglifts_program"
   subject { ProgramPage.new(program) }
-  let(:user) { create(:user, password: 'password') }
+  let(:user) { create(:user, password: "password") }
 
   before :each do
     login_page = LoginPage.new
     login_page.visit_page
-    login_page.login_with(user.username, 'password')
+    login_page.login_with(user.username, "password")
     subject.visit_page
   end
 

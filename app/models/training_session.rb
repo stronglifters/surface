b6b7746b1b45833544674ba7afb1bdab904e2c09
mo khan <mo@mokhan.ca>
2015-05-31 10:37:13 -0600

    move create_workout_from from training_session to android_import.

--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -13,43 +13,4 @@ class TrainingSession < ActiveRecord::Base
       sets: completed_sets
     )
   end
-
-  # refactor this to use the new api to add a workout
-  # training_session = user.begin(workout_a)
-  # training_session.train(squat, 200, [5,5,5,5,5])
-  def self.create_workout_from(workout_row, program)
-    transaction do
-      workout = program.workouts.find_by(name: workout_row.workout)
-      matching_workouts = where(occurred_at: workout_row.date)
-      if matching_workouts.any?
-        session = matching_workouts.first
-      else
-        session = create!(
-          workout: workout,
-          occurred_at: workout_row.date,
-          body_weight: workout_row.body_weight.to_f
-        )
-      end
-
-      session.exercise_sessions.destroy_all
-      workout.exercise_workouts.each_with_index do |exercise_workout, index|
-        exercise_row = workout_row.exercises[index]
-        sets = []
-        1.upto(exercise_workout.sets).each do |n|
-          if exercise_row["set#{n}"].to_i > 0
-            sets << exercise_row["set#{n}"]
-          else
-            sets << 0
-          end
-        end
-
-        session.exercise_sessions.create!(
-          target_weight: exercise_row["warmup"]["targetWeight"],
-          exercise_workout: exercise_workout,
-          sets: sets
-        )
-      end
-      session
-    end
-  end
 end

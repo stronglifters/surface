09760e6cf35d3ff9737ee53e6e726328a285ffb0
mo khan <mo@mokhan.ca>
2015-05-31 21:43:45 -0600

    key by date rather than epoch.

--- spec/models/training_history_spec.rb
+++ spec/models/training_history_spec.rb
@@ -18,7 +18,8 @@ describe TrainingHistory do
     it "returns the history in the format required for the chart" do
       result = subject.to_line_chart
       expect(result).to_not be_nil
-      expect(result[date.to_i]).to eql(target_weight.to_f)
+      expect(result.keys.first.to_i).to eql(date.to_i)
+      expect(result.values.first).to eql(target_weight.to_f)
     end
   end
 end

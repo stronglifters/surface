367421fb566fd7479e1dcd8b557add0520b9d4b1
mo khan <mo@mokhan.ca>
2015-06-01 20:39:48 -0600

    restart puma after publishing.

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,13 +57,19 @@ set :rbenv_type, :system
 set :rbenv_ruby, "2.2.2"
 
 namespace :deploy do
+  task :restart do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      execute :service, "puma restart"
+    end
+  end
+
+  after :publishing, :restart
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, "cache:clear"
       # end
-      execute :service, "puma restart"
     end
   end
 end

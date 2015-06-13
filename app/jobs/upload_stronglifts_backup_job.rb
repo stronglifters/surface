13e9724ca9312cdaf042060d868ac280a33c3f11
mo khan <mo@mokhan.ca>
2015-06-13 16:55:34 -0600

    fail gracefully for unknown file types.

--- app/jobs/upload_stronglifts_backup_job.rb
+++ app/jobs/upload_stronglifts_backup_job.rb
@@ -4,7 +4,9 @@ class UploadStrongliftsBackupJob < ActiveJob::Base
   def perform(user, backup_file, program)
     tmp_dir do |dir|
       `unzip #{backup_file} -d #{dir}`
-      importer_for(dir, user, program).import_from(dir)
+      ActiveRecord::Base.transaction do
+        importer_for(dir, user, program).import_from(dir)
+      end
     end
   end
 
@@ -19,7 +21,17 @@ class UploadStrongliftsBackupJob < ActiveJob::Base
   def importer_for(directory, user, program)
     [
       Android::Import.new(user, program),
-      Ios::Import.new(user, program)
+      Ios::Import.new(user, program),
+      UnknownFile.new
     ].detect { |x| x.can_parse?(directory) }
+  end
+
+  class UnknownFile
+    def can_parse?(directory)
+      true
+    end
+
+    def import_from(directory)
+    end
   end
 end

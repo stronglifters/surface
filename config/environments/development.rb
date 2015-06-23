15e11d83484597b11e3b0a14f93815afd773bb12
mo khan <mo@mokhan.ca>
2015-06-22 19:45:39 -0600

    enable bullet in development mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
     authentication:       :login,
     enable_starttls_auto: true
   }
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.console = true
+  end
 end

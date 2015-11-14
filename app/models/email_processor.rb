88dc8be8f500e15d26e82c6f3fd4c77f05b951b8
mo khan <mo@mokhan.ca>
2015-11-14 08:43:37 -0700

    process csv exports received via email.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,9 +1,15 @@
 class EmailProcessor
+  attr_reader :email
+
   def initialize(email)
     @email = email
   end
 
   def process
-    Rails.logger.info(@email.inspect)
+    user = User.find_by!(email: email.from[:email])
+
+    email.attachments.each do |attachment|
+      BackupFile.new(user, attachment).process_later(Program.stronglifts)
+    end
   end
 end
---
+++ spec/models/email_processor.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe EmailProcessor do
+  subject { EmailProcessor.new(email) }
+  let(:email) { build(:email, :with_attachment) }
+
+  context "with a csv attachment" do
+    let(:backup_file) { double(process_later: true) }
+    let(:user) { create(:user) }
+
+    before :each do
+      email.from[:email] = user.email
+    end
+
+    it "imports the csv attachment" do
+      allow(BackupFile).to receive(:new)
+        .with(user, email.attachments.first)
+        .and_return(backup_file)
+
+      subject.process
+
+      expect(backup_file).to have_received(:process_later).with(Program.stronglifts)
+    end
+  end
+end

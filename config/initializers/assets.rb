7d86f6ba4846a0202fed7fe397a9d9aaec61c36f
mo khan <mo@mokhan.ca>
2015-12-21 10:21:14 -0700

    remove adding files to assets.rb for teaspoon.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
-Rails.application.config.assets.precompile += %w( support/bind-poly.self.js )

10f02ec386547478a54f6b4622fcb50581fabf27
mo khan <mo@mokhan.ca>
2016-04-29 23:09:39 -0600

    list gyms in a table.

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,9 +4,10 @@ describe Location do
   describe '.from' do
     it 'returns the correct lat/long' do
       latitude, longitude = Location.
-        from('1817 Crowchild Trail NW', 'Calgary', 'AB', 'Canada')
-      expect(latitude).to be_within(0.1).of(51.0706973)
-      expect(longitude).to be_within(0.1).of(-114.1178249)
+        from('1301 16 Ave NW', 'Calgary', 'AB', 'Canada')
+
+      expect(latitude).to be_within(0.1).of(51.0647815)
+      expect(longitude).to be_within(0.1).of(-114.0927691)
     end
   end
 end

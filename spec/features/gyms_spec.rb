e92ce6d3d9edd0c788d69504b824e2d3d608ea1e
mo khan <mo@mokhan.ca>
2016-05-01 18:00:44 -0600

    add specs for adding a new gym.

--- spec/features/gyms_spec.rb
+++ spec/features/gyms_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 feature "Gyms", type: :feature do
+  let(:user_session) { create(:active_session, location: create(:calgary)) }
+
+  before :each do
+    page.set_rack_session(user_id: user_session.id)
+  end
+
   feature "viewing gyms" do
     subject { GymsPage.new }
     let!(:calgary_gym) { create(:gym, name: 'sait', location: create(:calgary)) }
     let!(:edmonton_gym) { create(:gym, name: 'nait', location: create(:edmonton)) }
-    let(:user_session) { create(:active_session, location: create(:calgary)) }
-
-    before :each do
-      page.set_rack_session(user_id: user_session.id)
-    end
 
     it 'loads the gyms closest to you' do
       subject.visit_page
@@ -24,6 +25,32 @@ feature "Gyms", type: :feature do
       expect(subject).to be_on_page
       expect(subject).to have_content(calgary_gym.name)
       expect(subject).to have_content(edmonton_gym.name)
+    end
+  end
+
+  feature "adding a gym" do
+    subject { NewGymPage.new }
+
+    it 'saves a new gym' do
+      subject.visit_page
+      subject.change(
+        name: 'SAIT',
+        address: '1301 16 Ave NW',
+        city: 'Calgary',
+        region: 'AB',
+        country: 'CA',
+        postal_code: 'T2M 0L4',
+      )
+
+      expect(Gym.count).to eql(1)
+      gym = Gym.last
+      expect(gym.name).to eql('SAIT')
+      expect(gym.location).to be_present
+      expect(gym.location.address).to eql('1301 16 Ave NW')
+      expect(gym.location.city).to eql('Calgary')
+      expect(gym.location.region).to eql('AB')
+      expect(gym.location.country).to eql('CA')
+      expect(gym.location.postal_code).to eql('T2M 0L4')
     end
   end
 end

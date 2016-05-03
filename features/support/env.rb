8db9850060ec13b6db74eb66b3a5af7dcefd491b
mo khan <mo@mokhan.ca>
2016-05-02 22:50:02 -0600

    attempt to submit form without js.

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@
 # files.
 
 require 'cucumber/rails'
+require "capybara/poltergeist"
 
 $flipper.enable(:gym)
 # Capybara defaults to CSS3 selectors rather than XPath.
@@ -46,6 +47,7 @@ DatabaseCleaner.strategy = :transaction
 #   end
 #
 
+#Capybara.default_driver = :poltergeist
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature

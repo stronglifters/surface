37522868c812b392d49394271ffce1cbcdc82a10
mo khan <mo@mokhan.ca>
2016-05-21 09:49:22 -0600

    add unique constraint to yelp_id index.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515023751) do
+ActiveRecord::Schema.define(version: 20160521154610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20160515023751) do
     t.string   "yelp_id"
   end
 
-  add_index "gyms", ["yelp_id"], name: "index_gyms_on_yelp_id", using: :btree
+  add_index "gyms", ["yelp_id"], name: "index_gyms_on_yelp_id", unique: true, using: :btree
 
   create_table "locations", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.uuid     "locatable_id"

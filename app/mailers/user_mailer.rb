849a808749d01e368eb142df92c6dba216b171c4
speasley <s@stephenpeasley.com>
2016-07-04 18:37:34 -0600

    Stronglifters -> Strong Lifters

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def registration_email(user)
     @username = user.username
-    mail to: user.email, subject: "Welcome to Stronglifters."
+    mail to: user.email, subject: "Welcome to Strong Lifters."
   end
 end

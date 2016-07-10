dc349a90684821530745fe95d1c4b398a04c663c
mo khan <mo@mokhan.ca>
2016-07-09 19:14:38 -0600

    declare attributes on recommendation model.

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,4 +1,8 @@
 class Recommendation < ApplicationRecord
+  attribute :duration, :integer
+  attribute :sets, :integer
+  attribute :repetitions, :integer
+  attribute :duration, :integer
   belongs_to :exercise
   belongs_to :routine
   delegate :name, to: :exercise

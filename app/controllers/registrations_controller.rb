6d6a3fc06e2ce88404afc0ca6a4a7b2c09e8a6ad
mo khan <mo@mokhan.ca>
2017-02-12 11:25:12 -0700

    use redirect instead of render to fix url.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,8 +16,8 @@ class RegistrationsController < PublicController
       flash[:notice] = translate(".success")
       redirect_to edit_profile_path(@user.username)
     else
-      flash.now[:error] = @user.errors.full_messages
-      render :new
+      flash[:error] = @user.errors.full_messages
+      redirect_to new_registration_path
     end
   end
 

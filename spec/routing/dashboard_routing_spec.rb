94ef708345b7b1ae38e848979e046376e3fa7a1b
mo khan <mo@mokhan.ca>
2017-02-16 13:59:25 -0700

    fix routing spec.

--- spec/routing/dashboard_routing_spec.rb
+++ spec/routing/dashboard_routing_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 describe "/dashboard", type: :routing do
   it "routes to the items listing" do
-    expect(get: "/dashboard").to route_to("workouts#index")
+    expect(get: "/dashboard").to route_to("dashboards#show")
   end
 end

03d4ad784221f39b2468ff346445b748eddc6c98
mo khan <mo@mokhan.ca>
2017-02-16 14:10:08 -0700

    remove spec for feature that is removed.

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -39,7 +39,7 @@ feature "Profiles", type: :feature do
       )
     end
 
-    it "allows me to choose my home gym", js: true do
+    xit "allows me to choose my home gym", js: true do
       gym = build(:gym)
       allow(Gym).to receive(:create_from_yelp!).and_return(gym)
 
